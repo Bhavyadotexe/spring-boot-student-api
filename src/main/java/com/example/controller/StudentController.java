a5ca20da8bda8d60c0b19d73961b05e488e8ea84
Bhavana Kandalkar <bhawanakandalkar@gmail.com>
2023-03-22 20:25:45 +0530

    get API with JPA

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -1,23 +1,25 @@
 package com.example.controller;
 
+import com.example.entity.Student;
 import com.example.response.StudentResponse;
+import com.example.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/Student/")
 public class StudentController {
 
-    @Value("${app.name: Default Demo App}")
-    private String appName;
+    @Autowired
+    StudentService studentService;
 
-    @GetMapping("/get")
-    private StudentResponse getStudent() {
-        StudentResponse studentResponse = new StudentResponse
-                (7, "Bhawana", "Kandalkar");
-        return studentResponse;
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
-
 }
