0acea620c030710aac685a3737e021e02b3ea2cf
Bhavana Kandalkar <bhawanakandalkar@gmail.com>
2023-03-23 22:36:40 +0530

    created the jpql query

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.entity.Student;
 import com.example.request.CreateStudentRequest;
+import com.example.request.InQueryRequest;
 import com.example.request.UpdateStudentRequest;
 import com.example.response.StudentResponse;
 import com.example.service.StudentService;
@@ -31,25 +32,99 @@ public class StudentController {
     }
 
     @PostMapping("create")
-    public StudentResponse createStudent(@Valid @RequestBody CreateStudentRequest studentRequest){
+    public StudentResponse createStudent(@Valid @RequestBody CreateStudentRequest studentRequest) {
         Student student = studentService.createStudent(studentRequest);
         return new StudentResponse(student);
     }
 
     @PutMapping("update")
-    public StudentResponse UpdateStudent(@Valid @RequestBody UpdateStudentRequest updateStudentRequest){
+    public StudentResponse UpdateStudent(@Valid @RequestBody UpdateStudentRequest updateStudentRequest) {
         Student student = studentService.updateStudent(updateStudentRequest);
         return new StudentResponse(student);
     }
 
-   /* @DeleteMapping("delete")
-        public String deleteStudent(@RequestParam("id") long id){
-        return studentService.deleteStudent(id);
-    }*/
+    /* @DeleteMapping("delete")
+         public String deleteStudent(@RequestParam("id") long id){
+         return studentService.deleteStudent(id);
+     }*/
     @DeleteMapping("/delete/{id}")
-    public String deleteStudent (@PathVariable long id){
+    public String deleteStudent(@PathVariable long id) {
         return studentService.deleteStudent(id);
     }
+
+    @GetMapping("getByFirstName/{firstName}")
+    public List<StudentResponse> getByFirstName(@PathVariable String firstName) {
+        List<Student> studentList = studentService.getByFirstName(firstName);
+
+        List<StudentResponse> studentResponseList = new ArrayList<StudentResponse>();
+
+        studentList.stream().forEach(student -> {
+            studentResponseList.add(new StudentResponse(student));
+        });
+        return studentResponseList;
     }
+
+    @GetMapping("getByFirstNameAndLastName/{firstName}/{lastName}")
+    public StudentResponse getByFirstNameAndLastName(@PathVariable String firstName,
+                                                           @PathVariable String lastName) {
+        return new StudentResponse(studentService.getByFirstNametAndLastName(firstName, lastName));
+    }
+
+    @GetMapping("getByFirstNameOrLastName/{firstName}/{lastName}")
+    public List<StudentResponse> getByFirstNameOrLastName(@PathVariable String firstName,
+                                                          @PathVariable String lastName) {
+        List<Student> studentList = studentService.getByFirstNametOrLastName(firstName, lastName);
+        List<StudentResponse> studentResponseList = new ArrayList<StudentResponse>();
+
+        studentList.stream().forEach(student -> {
+            studentResponseList.add(new StudentResponse(student));
+        });
+        return studentResponseList;
+    }
+
+    @GetMapping("getByFirstNameIn")
+    public List<StudentResponse> getByFirstNameIn(
+            @RequestBody InQueryRequest inQueryRequest) {
+        List<Student> studentList = studentService.getByFirstNameIn(inQueryRequest);
+        List<StudentResponse> studentResponseList = new ArrayList<StudentResponse>();
+
+        studentList.stream().forEach(student -> {
+            studentResponseList.add(new StudentResponse(student));
+        });
+        return studentResponseList;
+    }
+
+    @GetMapping("getAllWithPagination")
+    public List<StudentResponse> getAllStudentWithPagination(@RequestParam int pageNo,
+                                                             @RequestParam int pageSize) {
+        List<Student> studentList = studentService
+                .getAllStudentWithPagination(pageNo, pageSize);
+        List<StudentResponse> studentResponseList = new ArrayList<StudentResponse>();
+        studentList.stream().forEach(student -> {
+            studentResponseList.add(new StudentResponse(student));
+        });
+        return studentResponseList;
+    }
+
+    public List<StudentResponse> getAllStudentWithSorting() {
+        List<Student> studentList = studentService.getAllStudentWithSorting();
+        List<StudentResponse> studentResponseList = new ArrayList<StudentResponse>();
+        studentList.stream().forEach(student -> {
+            studentResponseList.add(new StudentResponse(student));
+        });
+        return studentResponseList;
+    }
+    @PutMapping("updateFirstName/{id}/{firstName}")
+    public String updateStudentWithJpql (@PathVariable Long id, @PathVariable String firstName){
+        return studentService.updateStudentWithJpql(id, firstName)+ "Student(s) updated";
+    }
+
+    @DeleteMapping("deleteByFirstName/{firstName}")
+    public String deleteStudent (@PathVariable String firstName){
+        return studentService.deleteStudent(firstName) + "Student(s) deleted";
+    }
+
+}
+
 
 
--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -3,40 +3,80 @@ package com.example.service;
 import com.example.entity.Student;
 import com.example.repository.StudentRepository;
 import com.example.request.CreateStudentRequest;
+import com.example.request.InQueryRequest;
 import com.example.request.UpdateStudentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class   StudentService {
+public class StudentService {
 
     @Autowired
     StudentRepository studentRepository;
 
-    public List<Student> getAllStudents(){
+    public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
 
-    public Student createStudent(CreateStudentRequest createStudentRequest){
-    Student student = new Student(createStudentRequest);
-    student = studentRepository.save(student);
-    return student;
+    public Student createStudent(CreateStudentRequest createStudentRequest) {
+        Student student = new Student(createStudentRequest);
+        student = studentRepository.save(student);
+        return student;
     }
-    public Student updateStudent(UpdateStudentRequest updateStudentRequest){
+
+    public Student updateStudent(UpdateStudentRequest updateStudentRequest) {
         Student student = studentRepository.findById(updateStudentRequest.getId()).get();
 
         if (updateStudentRequest.getFirstName() != null &&
-        !updateStudentRequest.getFirstName().isEmpty()){
+                !updateStudentRequest.getFirstName().isEmpty()) {
             student.setFirstName(updateStudentRequest.getFirstName());
         }
         student = studentRepository.save(student);
         return student;
     }
-     public String deleteStudent (long id) {
+
+    public String deleteStudent(long id) {
         studentRepository.deleteById(id);
         return "Student Has been deleted Successfully";
-     }
+    }
 
+    public List<Student> getByFirstName(String firstName) {
+        return studentRepository.findByFirstName(firstName);
+    }
+
+    public Student getByFirstNametAndLastName(String firstName, String lastName) {
+       //return studentRepository.findByFirstNameAndLastName(firstName, lastName);
+        return studentRepository.getByFirstNameAndLastName(firstName, lastName);
+
+    }
+
+    public List<Student> getByFirstNametOrLastName(String firstName, String lastName) {
+        return studentRepository.findByFirstNameOrLastName(firstName, lastName);
+    }
+
+    public List<Student> getByFirstNameIn(InQueryRequest inQueryRequest) {
+        return studentRepository.findByFirstNameIn(inQueryRequest.getFirstNames());
+    }
+
+    public List<Student> getAllStudentWithPagination(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return studentRepository.findAll(pageable).getContent();
+    }
+
+    public List<Student> getAllStudentWithSorting() {
+        Sort sort = Sort.by(Sort.Direction.ASC, "firstName", "lastName");
+        return studentRepository.findAll(sort);
+    }
+
+    public Integer updateStudentWithJpql(Long id, String firstName) {
+        return studentRepository.updateFirstName(id, firstName);
+    }
+    public Integer deleteStudent (String firstName){
+    return studentRepository.deleteByFirstName(firstName);
+    }
 }
